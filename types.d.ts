7e34f4866ab99874913e98e0a2c99e3c2d9e0094
Ravi <ravi.k@imsolutions.mobi>
2025-06-05 14:28:40 +0530

    commit

--- types.d.ts
+++ types.d.ts
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 declare module 'next' {
   export interface PageProps {
     params?: Record<string, string>;
-    searchParams?: Record<string, string | string[]>;
+    /*searchParams?: Record<string, string | string[]>;*/
     children?: ReactNode;
   }
 }
